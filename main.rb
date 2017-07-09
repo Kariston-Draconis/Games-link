99439beea5a0a2aee1d53d8299909e2de5808148
Alex May <thrawnrulz@hotmail.com>
2017-07-09 15:55:19 +1000

    buttons are starting to work

--- main.rb
+++ main.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 require 'pg'
+require_relative 'db_config'
+require_relative 'models/user'
+
+enable :sessions
 
 def run_sql(sql)
   conn = PG.connect(dbname: 'games_link')
@@ -12,4 +16,22 @@ end
 
 get '/' do
   erb :index
+end
+
+post '/login' do
+  erb :login
+end
+
+post '/register' do
+  erb :register
+end
+
+post '/sessions' do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/main'
+  else
+    erb :index
+  end
 end
